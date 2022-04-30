b0a0b4abb18c2280d6193bf3651d9d6c7eac2076
Alec Dziwanowski <adziwanowski@gmail.com>
2022-04-29 23:13:58 -0400

    working async get and post requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,15 +6,17 @@ const fs = require('fs/promises');
 const uuid = require('../helpers/uuid');
 
 // GET /api/notes - read the db.json file and return all saved notes as JSON.
-api.get('/', (request, response) => {
-    fs.readFile('./db/db.json')
-    .then(data => response.json(JSON.parse(data)))
-    // if there is an error, tell the user
-    .catch(() => response.status(500).json('Error in saving note.'));
+api.get('/', async (request, response) => {
+    try {
+        const savedNotes = await fs.readFile('./db/db.json');
+        return response.json(JSON.parse(savedNotes));
+    } catch {
+        return response.status(500).json('Error in saving note.');
+    }
 });
 
 // POST /api/notes - adds body to db.json
-api.post('/', (request, response) => {
+api.post('/', async (request, response) => {
     // destructure req.body
     const { title, text } = request.body;
     
@@ -29,23 +31,53 @@ api.post('/', (request, response) => {
         };
 
         // obtain existing notes
-        fs.readFile('./db/db.json')
-        // return notes with newNote added
-        .then(data => {
-            const parsedNotes = JSON.parse(data);
+        try {
+            const savedNotes = await fs.readFile('./db/db.json');
+            const parsedNotes = JSON.parse(savedNotes);
             // add newNote to db.json file
             parsedNotes.push(newNote);
-            return parsedNotes;
-        })
-        // write updated notes to the db.json file
-        .then(parsedNotes => {
-            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes));
+            const parsedNotesNew = parsedNotes;
+            fs.writeFile('./db/db.json', JSON.stringify(parsedNotesNew));
             response.status(201).json('Success!');
-        })
-        // if there is an error, tell the user
-        .catch(() => response.status(500).json('Error in saving note.'));
+        } catch {
+            return response.status(500).json('Error in saving note.');
+        }
     }
 });
+
+// * `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll 
+// need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+// async await
+
+// Delete /api/notes/id - deletes note
+// api.delete('/:id', (request, response) => {
+//     // destructure req.body
+//     const { title, text, id } = request.body;
+// 
+//     // if all required properties are given
+//     // if (title && text && id) {
+//     //     // establish properties of a note
+//     //     const newNote = {
+//     //         title,
+//     //         text,
+//     //         // gives each note a unique id
+//     //         id: uuid(),
+//     //     };
+
+//         // obtain existing notes
+//         fs.readFile('./db/db.json')
+//         // return notes with newNote added
+//         .then(data => JSON.parse(data))
+//         // write updated notes to the db.json file
+//         .then(parsedNotes => {
+//             fs.writeFile('./db/db.json', JSON.stringify(parsedNotes));
+//             response.status(201).json('Success!');
+//         })
+//         // if there is an error, tell the user
+//         .catch(() => response.status(500).json('Error in saving note.'));
+//     }
+// });
 
 // exports api so that it can be accessed at /api/notes path
 module.exports = api;
